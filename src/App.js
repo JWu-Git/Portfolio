aede6230e5bd74b14c11845743a6b57707f40315
JWu-Git <87672250+JWu-Git@users.noreply.github.com>
2023-08-02 12:30:10 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Alert } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {
+        <Alert key="primary" variant="primary">
+          This is a primary alert—check it out!
+        </Alert>
+      }
+    </>
   );
 }
 
