1c560da0f7fe32e825780658751d39891ffba30a
JWu-Git <87672250+JWu-Git@users.noreply.github.com>
2023-08-02 22:41:02 -0400

    bubble sort works

--- src/pages/Sorting/Bubble.js
+++ src/pages/Sorting/Bubble.js
@@ -3,10 +3,25 @@ import Input from '../../components/Input';
 import DisplayNumbers from '../../components/DisplayNumbers';
 import { bubbleSort, setBubbleData } from '../../store/index';
 import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 export default function Bubble() {
   const data = useSelector((data) => data.values);
+
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const delayedBubbleSort = async () => {
+      for (let i = data.length - 1; i > 0; i--) {
+        for (let j = 0; j <= i - 1; j++) {
+          await new Promise((resolve) => setTimeout(resolve, 100));
+          dispatch(bubbleSort(j));
+        }
+      }
+    };
+
+    delayedBubbleSort();
+  }, [data]);
 
   const handleSetData = (values) => {
     const numbersArray = values.split(',').map((num) => parseInt(num));
--- src/store/slices/bubble.js
+++ src/store/slices/bubble.js
@@ -6,15 +6,13 @@ const bubbleSlice = createSlice({
   name: 'bubble',
   initialState,
   reducers: {
-    bubbleSort(state) {
-      for (let i = state.values.length - 1; i > 0; i--) {
-        for (let j = 0; j <= i - 1; j++) {
-          if (state.values[j] > state.values[j + 1]) {
-            let temp = state.values[j + 1];
-            state.values[j + 1] = state.values[j];
-            state.values[j] = temp;
-          }
-        }
+    bubbleSort(state, action) {
+      const j = action.payload;
+      //payload is object j
+      if (state.values[j] > state.values[j + 1]) {
+        let temp = state.values[j + 1];
+        state.values[j + 1] = state.values[j];
+        state.values[j] = temp;
       }
     },
     setBubbleData(state, action) {
